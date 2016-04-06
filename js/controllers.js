1930e0c805148d40bb3226699261c0517d872b38
RachelKoldenhoven <rachel@koldenfrozen.com>
2016-04-06 11:16:48 -0600

    pokemon displays on page

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,35 @@
 /**
  * Created by rachelkoldenhoven on 4/6/16.
  */
-app.controller('PokemonController', function($scope) {
+app.controller('PokemonController', function($scope, $http) {
   $scope.view = {};
   $scope.view.display = "";
-  $scope.newItem = {};
-});
+  $scope.newPokemon = {};
+  $scope.newMove = {};
+  $scope.pokemonId = 1;
+
+  $scope.getRandom = function() {
+
+  };
+
+  $scope.getNewPokemon = function() {
+    $scope.loading = true;
+    $scope.pokemonId = Math.floor((Math.random() * 721) + 1);
+    $http.get('http://pokeapi.co//api/v2/pokemon/' + $scope.pokemonId).then(function(data){
+      $scope.newPokemon = data.data;
+      $scope.loading = false;
+      console.log($scope.newPokemon);
+    });
+  };
+
+  $scope.getNewMove = function() {
+    $scope.pokemonId = Math.floor((Math.random() * 639) + 1);
+    $http.get('http://pokeapi.co//api/v2/move/' + $scope.pokemonId).then(function(data){
+      $scope.newMove = data.data;
+      console.log($scope.newMove.name);
+    });
+  };
+
+
+
+  });
--- js/directives.js
+++ js/directives.js
@@ -1,3 +1,35 @@
 /**
  * Created by rachelkoldenhoven on 4/6/16.
  */
+angular.module('Pokemon').directive('loading', function () {
+    return {
+      restrict: 'E',
+      replace:true,
+      template: '<div class="loading"><img src="http://ringophone.com/HDanimWP/Loading-87197.gif"/>LOADING...</div>',
+      link: function (scope, element, attr) {
+        scope.$watch('loading', function (val) {
+          if (val)
+            $(element).show();
+          else
+            $(element).hide();
+        });
+      }
+    }
+  });
+
+//
+//angular.module('SimpleDirectiveApp').directive('isolateScopeCustomer', function() {
+//  return {
+//    scope: {
+//      customerValues: '=customerValues',
+//    },
+//    templateUrl: '/templates/isolateScopeTemplate.html',
+//    link: function(scope, element, attrs) {
+//      scope.clickCount = 0;
+//
+//      scope.handleClick = function(){
+//        scope.clickCount += 1;
+//      }
+//    }
+//  };
+//});
